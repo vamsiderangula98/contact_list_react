bc7907405af19bad2b8e7982906c9dcc0b138eef
VAMSI KRISHNA DERANGULA <41372768+vamsikrishna987@users.noreply.github.com>
2022-03-07 19:50:47 +0530

    adding contact card component functionality

---
+++ src/components/Contact.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import { Button, Card, ListGroupItem, ListGroup, Row } from 'react-bootstrap';
+import UpdateContact from './UpdateContact';
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showUpdateForm: false,
+      hideCard: false,
+    };
+  }
+  //hide update form
+  hideUpdateContactForm = () => {
+    this.setState({
+      showUpdateForm: false,
+      hideCard: false,
+    });
+  };
+  handleContactEdit = () => {
+    this.setState({
+      showUpdateForm: true,
+      hideCard: true,
+    });
+  };
+  handleDeleteContact = (userId) => {
+    let url = `https://jsonplaceholder.typicode.com/users/${userId}`;
+    fetch(url, {
+      method: 'DELETE',
+    }).then((res) => {
+      if (res.status === 200) {
+        this.props.deleteContact(userId);
+      }
+    });
+  };
+
+  render() {
+    const { user, editContact } = this.props;
+    const { id,name, email, phone, address } = user;
+    const { showUpdateForm, hideCard } = this.state;
+    const ColoredLine = ({ color }) => (
+      <hr
+        style={{
+          color: color,
+          backgroundColor: color,
+          height: 5,
+        }}
+      />
+    );
+    return (
+      <div>
+        <Row>
+          {' '}
+          {showUpdateForm ? (
+            <UpdateContact
+              updateContact={editContact}
+              user={user}
+              hideForm={this.hideUpdateContactForm}
+            />
+          ) : null}
+        </Row>
+        {!hideCard && (
+          <Row>
+            <Card
+              style={{ width: '22rem' }}
+              className="mt-3 contact-container"
+
+        
+            >
+              <Card.Body>
+      <img src="https://cdn-icons.flaticon.com/png/512/3177/premium/3177440.png?token=exp=1646658929~hmac=af11018bffbe2178187b4ce7cf2ced49" alt="profile-pic"></img>
+                <Card.Title className='card-title'>{name}</Card.Title>
+              </Card.Body>
+              <ColoredLine color="grey" />
+              
+              <ListGroup className="list-group-flush">
+                <ListGroupItem>Id :{id}</ListGroupItem>
+                <ListGroupItem>Name: {name}</ListGroupItem>
+                <ListGroupItem>Email :{email}</ListGroupItem>
+                <ListGroupItem>Phone:{phone}</ListGroupItem>
+                <ListGroupItem>Address :{address.city}</ListGroupItem>
+              </ListGroup>
+              <Card.Body>
+                <Button className="edit-btn"variant="success" onClick={this.handleContactEdit}>
+                  Edit
+                </Button>{' '}
+                <Button
+                className="delete-btn"
+                  variant="danger"
+                  // onClick={() => deleteContact(user)}
+                  onClick={() => this.handleDeleteContact(id)}
+                >
+                  Delete
+                </Button>
+              </Card.Body>
+            </Card>
+          </Row>
+        )}
+      </div>
+    );
+  }
+}
+export default Contact;
