f3d68daf042b56c4efd6a97d4fc5ee7692b069a4
VAMSI KRISHNA DERANGULA <41372768+vamsikrishna987@users.noreply.github.com>
2022-03-07 19:46:09 +0530

    adding styles to navbar component and its functionality

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+
+
+import React from 'react';
+import '../index.css';
+
+class NavBar extends React.Component{
+  render() {
+    return (
+      <div className='navbar-container'>
+          <div className='navbar-content'>
+          <h1 className="text-left text-success "> My Contact App</h1>
+          </div>
+        
+      </div>
+    )
+  }
+}
+export default NavBar;
+
